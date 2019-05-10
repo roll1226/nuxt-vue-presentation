07c95413bf76a17a05d68969e00628a43665ddd4
roll1226 <tomo_yasu_4724@icloud.com>
2019-05-10 11:38:47 +0900

    pwa

---
+++ docs/sw.js
@@ -0,0 +1,35 @@
+importScripts('/nuxt-vue-presentation/_nuxt/workbox.4c4f5ca6.js')
+
+workbox.precaching.precacheAndRoute([
+  {
+    "url": "/nuxt-vue-presentation/_nuxt/782ce9ffdc272b31c4c2.js",
+    "revision": "376d67de29ecb5963d6744455050934a"
+  },
+  {
+    "url": "/nuxt-vue-presentation/_nuxt/b3785e3790a4df3524e9.js",
+    "revision": "7292e29fcabd31d691d339465207c84b"
+  },
+  {
+    "url": "/nuxt-vue-presentation/_nuxt/be2b95ef307d1132c175.js",
+    "revision": "1b49b3b1ddb884c6916f699cc61b49af"
+  },
+  {
+    "url": "/nuxt-vue-presentation/_nuxt/bebe8aacb406bf857a2b.js",
+    "revision": "53b95f072d12c3d4b12574da99bcd22b"
+  },
+  {
+    "url": "/nuxt-vue-presentation/_nuxt/c085455a32a257eb38e9.js",
+    "revision": "bd667c184a3872454e063165067796c1"
+  }
+], {
+  "cacheId": "nuxtStudy",
+  "directoryIndex": "/",
+  "cleanUrls": false
+})
+
+workbox.clientsClaim()
+workbox.skipWaiting()
+
+workbox.routing.registerRoute(new RegExp('/nuxt-vue-presentation/_nuxt/.*'), workbox.strategies.cacheFirst({}), 'GET')
+
+workbox.routing.registerRoute(new RegExp('/nuxt-vue-presentation/.*'), workbox.strategies.networkFirst({}), 'GET')
